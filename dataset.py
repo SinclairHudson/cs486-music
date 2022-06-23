288e694b9d3b47c0b2fa4f3c9cf356e44b5af7be
Sinclair Hudson <hudson.sinclair00@gmail.com>
2022-06-23 10:38:30 -0400

    dataset set, successfully loading audio FTs in training loop

--- dataset.py
+++ dataset.py
@@ -2,36 +2,38 @@ import os
 from torch.utils.data import Dataset
 import torchaudio
 torchaudio.set_audio_backend("sox_io")
+from torchaudio.transforms import InverseSpectrogram, Spectrogram
 
 class LofiDataset(Dataset):
     """
     Dataset of audio clips, in a folder.
     """
-    def __init__(self, root_path: str, length=45):
+    def __init__(self, root_path: str,
+                 spectrogram: torchaudio.transforms.Spectrogram,
+                 length=45):
         super(LofiDataset).__init__()
         self.root_path = root_path
-        self.min_length = 45  # length in seconds
+        self.min_length = length  # length in seconds
         self.audio_files = os.listdir(root_path)
+        self.transform = spectrogram
 
         # filter audio_files by 
-        good = []
+        self.good_audio_files = []
         for i, audio_file in enumerate(self.audio_files):
             info = torchaudio.info(os.path.join(self.root_path, audio_file))
-            raw_audio = torchaudio.load(os.path.join(self.root_path, audio_file))
-            breakpoint()
+            length = info.num_frames / info.sample_rate  # length in seconds
             if length >= self.min_length:
-                # keep
+                self.good_audio_files.append(os.path.join(self.root_path, audio_file))
 
-                pass
-
-        # filter by the audio files that are of suitable length
-        self.audio_files = self.audio_files[good]
+        print(f"Initialized LofiDataset with {self.__len__()} songs " \
+              f"of length {self.min_length}.")
+        print(f"Total audio time is {self.__len__() * self.min_length} seconds.")
 
     def __len__(self):
-        return len(self.audio_files)
+        return len(self.good_audio_files)
 
     def  __getitem__(self, index):
-        raw_audio = torchaudio.load(self.audio_files[index])
-        crop = None # TODO crop the audio to min_length in length
-
-        return crop
+        raw_audio, sample_rate = torchaudio.load(self.good_audio_files[index])
+        stop = sample_rate * self.min_length
+        raw_audio = raw_audio[0][:stop]
+        return self.transform(raw_audio)
--- train_compressor.py
+++ train_compressor.py
@@ -2,13 +2,27 @@ import torch
 from dataset import LofiDataset
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from torchaudio.transforms import InverseSpectrogram, Spectrogram
 
 
 N_EPOCHS = 100
+BATCH_SIZE = 4
 
-train_dataset = LofiDataset("/media/sinclair/datasets4/lofi/good_splits")
+spectrogram_params = {
+    "n_fft": 800,
+    "normalized": True,
+}
 
-train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+s = Spectrogram(**spectrogram_params)
+inv = InverseSpectrogram(**spectrogram_params)
+
+device = torch.device("cuda:0")
+train_dataset = LofiDataset("/media/sinclair/datasets4/lofi/good_splits",
+                            spectrogram=s,
+                            length=15)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
 for epoch in range(N_EPOCHS):
     for x in tqdm(train_loader):
-        print(wefe)
+        x.to(device)
+        breakpoint()
